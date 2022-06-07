0e461bc4c218c39e031b5a36743988048d01eb55
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-06-07 22:54:24 +0100

    Initial metamask connection and contract connection

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -3,6 +3,7 @@ import "../../styles/spinner.css";
 const Spinner = () => (
   <div className="flex items-center justify-center w-full h-full">
     <div className="loading-spinner" />
+    <p className="ml-5">Loading...</p>
   </div>
 );
 
