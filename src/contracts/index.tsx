568de16d8aa50dd1e68c054a0e170f755520c4ad
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-06-07 22:35:58 +0100

    Changed location of contacts

---
+++ src/contracts/index.tsx
@@ -0,0 +1,6 @@
+import MarketplaceAddress from "./Marketplace-address.json";
+import Marketplace from "./Marketplace.json";
+import NFTAdress from "./NFT-address.json";
+import NFT from "./NFT.json";
+
+export { Marketplace, MarketplaceAddress, NFT, NFTAdress };
