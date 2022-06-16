c5245e91f9f2e3c89d02d09e48acd3a7f3308595
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-06-16 17:05:22 +0200

    Fixed issue with CORS failing to fetch JSON

--- src/utils/constants/blockchain.tsx
+++ src/utils/constants/blockchain.tsx
@@ -1,3 +1,3 @@
-export const IPFS_BASE_URL = "http://ipfs.infura.io/ipfs/";
+export const IPFS_BASE_URL = "https://ipfs.infura.io/ipfs/";
 
 export const IPFS_CONNECTION_URL = "https://ipfs.infura.io:5001/api/v0";
