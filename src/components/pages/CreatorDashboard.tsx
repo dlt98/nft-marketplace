6e8bd0fb5a3d88f10db6d89f5a16d8b8fd3f53af
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-06-19 10:37:58 +0200

    Fix for items not displaying properly when sending and buying back and forth

--- src/components/pages/CreatorDashboard.tsx
+++ src/components/pages/CreatorDashboard.tsx
@@ -20,14 +20,27 @@ const CreatorDashboard = ({
     if (!account) return;
 
     // Fetch purchased items from marketplace by quering Offered events with the buyer set as the user
-    const filter = marketplace.filters.Offered(null, null, null, null, account);
-    const results = await marketplace.queryFilter(filter);
-
-    const soldItems: NFTtype[] = [];
+    const offeredFilter = marketplace.filters.Offered(
+      null,
+      null,
+      null,
+      null,
+      account
+    );
+    const boughtFilter = marketplace.filters.Bought(
+      null,
+      null,
+      null,
+      null,
+      account,
+      null
+    );
+    const offeredResults = await marketplace.queryFilter(offeredFilter);
+    const boughtResults = await marketplace.queryFilter(boughtFilter);
 
     //Fetch metadata of each nft and add that to listedItem object
     const listedItems: NFTtype[] = await Promise.all(
-      results.map(async (i: any) => {
+      offeredResults.map(async (i: any) => {
         //fetch arguments from each result
         i = i.args;
 
@@ -53,8 +66,36 @@ const CreatorDashboard = ({
           image: metadata.image,
         };
 
-        //Add listen item to sold
-        if (i.sold) soldItems.push(tempItem);
+        return tempItem;
+      })
+    );
+
+    const soldItems: NFTtype[] = await Promise.all(
+      boughtResults.map(async (i: any) => {
+        //fetch arguments from each result
+        i = i.args;
+
+        //get uri url from nft contract
+        const uri: string = await nft.tokenURI(i.tokenId);
+
+        //use uri to fetch the nft metadata stored on ipfs
+        const res = await fetch(uri);
+        const metadata = await res.json();
+
+        //get total price of item (item price + fee)
+        const totalPrice: BigNumberish = await marketplace.getTotalPrice(
+          i.itemId
+        );
+
+        //define listed item object
+        const tempItem: NFTtype = {
+          totalPrice,
+          price: i.price,
+          itemId: i.itemId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+        };
 
         return tempItem;
       })
@@ -64,48 +105,6 @@ const CreatorDashboard = ({
     setListedItems(listedItems);
     setSoldItems(soldItems);
   };
-  // const loadNFTs = async () => {
-  //   const itemCount = await marketplace.itemCount();
-  //   const listedItems: NFTtype[] = [];
-  //   const soldItems: NFTtype[] = [];
-
-  //   for (let i = 1; i <= itemCount; i++) {
-  //     const item = await marketplace.items(i);
-
-  //     if (item.seller.toLowerCase() === account) {
-  //       const uri: string = await nft.tokenURI(item.tokenId);
-  //       //use uri to fetch the nft metadata
-  //       const res = await fetch(uri);
-  //       const metadata = await res.json();
-
-  //       //get total price of item (item price + fee)
-  //       const totalPrice: BigNumberish = await marketplace.getTotalPrice(
-  //         item.itemId
-  //       );
-
-  //       //define listed item object
-  //       const tempItem: NFTtype = {
-  //         totalPrice,
-  //         price: item.price,
-  //         itemId: item.itemId,
-  //         name: metadata.name,
-  //         description: metadata.description,
-  //         image: metadata.image,
-  //       };
-
-  //       console.log("tempItem", tempItem);
-
-  //       listedItems.push(tempItem);
-
-  //       //Add listen item to sold
-  //       if (item.sold) soldItems.push(tempItem);
-  //     }
-
-  //     setIsLoading(false);
-  //     setListedItems(listedItems);
-  //     setSoldItems(soldItems);
-  //   }
-  // };
 
   if (isLoading) return <Spinner label="Loading marketplace items..." />;
 
